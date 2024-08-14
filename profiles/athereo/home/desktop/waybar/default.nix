7c2579977c1e9f97568e6591898d0405a106cb67
Athereo <athereoandromeda@gmail.com>
2024-08-15 00:53:24 +0800

    refactor/style/waybar-scss: Add Base16 module

--- profiles/athereo/home/desktop/waybar/default.nix
+++ profiles/athereo/home/desktop/waybar/default.nix
@@ -1,12 +1,68 @@
 {
   config,
-  inputs,
+  # inputs,
+  pkgs,
   ...
-}: {
+}: let
+  base16-text = with config.scheme.withHashtag; ''
+    $base00: ${base00};
+    $base01: ${base01};
+    $base02: ${base02};
+    $base03: ${base03};
+    $base04: ${base04};
+    $base05: ${base05};
+    $base06: ${base06};
+    $base07: ${base07};
+    $base08: ${base08};
+    $base09: ${base09};
+    $base0A: ${base0A};
+    $base0B: ${base0B};
+    $base0C: ${base0C};
+    $base0D: ${base0D};
+    $base0E: ${base0E};
+    $base0F: ${base0F};
+  '';
+
+  build-scss = pkgs.stdenv.mkDerivation {
+    name = "sass-builder";
+    src = ./theme-1/scss;
+
+    nativeBuildInputs = with pkgs; [
+      dart-sass
+      python312
+    ];
+
+    dontUnpack = true;
+    dontPatch = true;
+    dontConfigure = true;
+    dontInstall = true;
+
+    buildPhase = ''
+      mkdir -p $out
+      mkdir build
+
+      # find $src -depth -type f ! -name "base16.scss" -exec cp {} build \;
+      # find "$src" -type f ! -name "base16.scss" -execdir cp {} "build"/{} \;
+      cd $src
+      python filter.py
+      cd ..
+      echo '${base16-text}' > build/_base16.scss;
+
+      ls build
+
+      echo "Building SCSS..."
+      sass build/:$out/dist
+    '';
+  };
+in {
   programs.waybar.enable = true;
 
   xdg.configFile = {
-    "waybar/scheme.css".text = builtins.readFile (config.scheme inputs.base16-waybar);
+    "waybar/dist" = {
+      recursive = true;
+      source = "${build-scss}/dist";
+    };
+    # "waybar/scheme.css".text = builtins.readFile (config.scheme inputs.base16-waybar);
     "waybar/theme-1".source = ./theme-1;
   };
 }
