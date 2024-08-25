78de51bd177902a4ba5ab741e6d30a855a750b36
Athereo <athereoandromeda@gmail.com>
2024-08-25 21:26:34 +0800

    style/pkgs(wezterm): Change padding

--- profiles/athereo/home/cli/terminals/wezterm/wezterm.lua
+++ profiles/athereo/home/cli/terminals/wezterm/wezterm.lua
@@ -4,6 +4,7 @@ local wezterm = require 'wezterm'
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
+-- This is a workaround until the issue is fixed
 if os.getenv("XDG_CURRENT_DESKTOP") == "Hyprland" then
   config.enable_wayland = false
 else
@@ -12,6 +13,17 @@ end
 
 config.window_background_opacity = 0.4
 config.font = wezterm.font("JetBrainsMono Nerd Font")
-config.font_size = 10
+config.font_size = 10.5
+-- config.enable_tab_bar = false
+-- config.window_decorations = "RESIZE"
+config.enable_scroll_bar = true
+
+config.window_padding = {
+  left = 2,
+  right = 12,
+  top = 0,
+  bottom = 0,
+}
+
 
 return config
