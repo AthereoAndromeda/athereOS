d24ecb46fb5f9abc4034ef8b84b2e28a1df0abd6
Athereo <athereoandromeda@gmail.com>
2024-08-26 16:06:33 +0800

    feat/pkgs(yazi): Add close keybind

--- profiles/athereo/home/cli/apps/yazi.nix
+++ profiles/athereo/home/cli/apps/yazi.nix
@@ -1,7 +1,24 @@
 {pkgs, ...}: {
-  home.packages = with pkgs; [
-    yazi
+  programs.yazi = {
+    enable = true;
+    enableBashIntegration = true;
 
+    keymap = {
+      manager.prepend_keymap = [
+        {
+          on = ["c"];
+          run = "close";
+          desc = "Close tab";
+        }
+      ];
+    };
+
+    # theme = {
+    #   flavor = { use = "rose pine";}; crystal
+    # };
+  };
+
+  home.packages = with pkgs; [
     # TODO: Move to sys/utils.nix
     fd
     ripgrep
